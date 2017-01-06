46a8dc869eb629520ed8ac2b42312c6204c74a38
Rod Glover <rglover@uvic.ca>
2017-01-06 15:09:16 -0800

    Improve route definition

--- was/routes.py
+++ was/routes.py
@@ -1,4 +1,4 @@
-from werkzeug.routing import BaseConverter, IntegerConverter
+from werkzeug.routing import IntegerConverter
 from flask.ext.sqlalchemy import SQLAlchemy
 import was.api
 
@@ -8,15 +8,7 @@ class ValidYearConverter(IntegerConverter):
 
         Rule('/page/<valid_year:value>')
 
-    This converter does not support negative values.
-
     :param map: the :class:`Map`.
-    :param fixed_digits: the number of fixed digits in the URL.  If you set
-                         this to ``4`` for example, the application will
-                         only match if the url looks like ``/0001/``.  The
-                         default is variable length.
-    :param min: the minimal value.
-    :param max: the maximal value.
     """
     regex = r'\d+'
     num_convert = int
@@ -30,15 +22,7 @@ class MonthConverter(IntegerConverter):
 
         Rule('/page/<int_month:value>')
 
-    This converter does not support negative values.
-
     :param map: the :class:`Map`.
-    :param fixed_digits: the number of fixed digits in the URL.  If you set
-                         this to ``4`` for example, the application will
-                         only match if the url looks like ``/0001/``.  The
-                         default is variable length.
-    :param min: the minimal value.
-    :param max: the maximal value.
     """
     regex = r'\d+'
     num_convert = int
@@ -51,21 +35,19 @@ def add_routes(app):
     app.url_map.converters['valid_year'] = ValidYearConverter
     app.url_map.converters['int_month'] = MonthConverter
 
-    # Helpers for setting up routes
-    def dataset(name):
-        return '<any({}):dataset>'.format(name)
-    variable = '<any(tmax, tmin, precip):variable>'
-    month = '<int_month:month>'
-    # month = '<any({}):month>'.format(','.join("'{}'".format(m) for m in range(1, 13)))
+    def components(dataset):
+        """Helper for setting up routes"""
+        return {
+            'dataset': '<any({}):dataset>'.format(dataset),
+            'variable': '<any(tmax, tmin, precip):variable>',
+            'year': '<valid_year:year>',
+            'month': '<int_month:month>',
+        }
 
     db = SQLAlchemy(app)
 
-    @app.route('/{}/{};{}'.format(dataset('baseline'), variable, month),
-               methods=['GET'])
-    def baseline(**kwargs):
+    @app.route('/{dataset}/{variable};{month}'.format(**components('baseline')), methods=['GET'])
+    @app.route('/{dataset}/{variable};{year}-{month}'.format(**components('weather')), methods=['GET'])
+    def dispatch(**kwargs):
         return was.api.dispatch(db.session, **kwargs)
-
-    @app.route('/{}/{};<valid_year:year>-{}'.format(dataset('weather'), variable, month),
-               methods=['GET'])
-    def weather(**kwargs):
-        return was.api.dispatch(db.session, **kwargs)
+    # dispatch = partial(was.api.dispatch, db.session)  # rats, this doesn't work
